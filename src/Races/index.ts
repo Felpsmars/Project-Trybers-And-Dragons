c9463096135da3433171b7698508e3f8453c0534
Felipe Martins <mfelipeluiz99@gmail.com>
2022-03-15 16:59:16 -0300

    Req02: Creating races to inherit the race class

---
+++ src/Races/index.ts
@@ -0,0 +1,9 @@
+import Race from './Race';
+import Orc from './Orc';
+import Dwarf from './Dwarf';
+import Halfling from './Halfling';
+import Elf from './Elf';
+
+export { Orc, Dwarf, Halfling, Elf };
+
+export default Race;
